b6dc25dc9fd0574aab2db2384257e1cfc8e6a737
unknown <Gustavo Makimori>
2024-03-18 21:56:16 -0300

    Controller and domain cart initial

---
+++ cart/src/main/java/com/nextbuy/cart/domain/Cart.java
@@ -0,0 +1,25 @@
+package com.nextbuy.cart.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "cart")
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private List<Long> itemsId = new ArrayList<>();
+
+}
