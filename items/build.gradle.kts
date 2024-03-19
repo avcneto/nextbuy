575ee4004117bd54a016b2a1943d50897b09d573
Felipe Chimin <flpchimin@gmail.com>
2024-03-18 23:57:51 -0300

    Removida dependência jpa build.gradle - item

--- items/build.gradle.kts
+++ items/build.gradle.kts
@@ -25,7 +25,6 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-//	implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.2.3")
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb:3.2.3")
 
