4b8d036e05e25e43054ce34de84cb13c6d19f100
Deco <deco.neto10@gmail.com>
2024-03-22 20:28:33 -0300

    change variable name and order dependency

--- user/build.gradle.kts
+++ user/build.gradle.kts
@@ -36,8 +36,8 @@ dependencies {
 	implementation("org.apache.commons:commons-lang3:3.14.0")
 	implementation("io.jsonwebtoken:jjwt-api:0.12.5")
 	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.5")
-	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.4.0")
 	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.4.0")
 }
 
 tasks.withType<Test> {
