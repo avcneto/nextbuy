0b3e33d961c56abcc10cac56cbada9ea735628aa
Deco <deco.neto10@gmail.com>
2024-03-25 23:01:49 -0300

    wip

--- cart/src/main/java/com/nextbuy/cart/service/CartService.java
+++ cart/src/main/java/com/nextbuy/cart/service/CartService.java
@@ -21,8 +21,6 @@ import java.math.BigDecimal;
 import java.util.HashSet;
 import java.util.List;
 
-import static com.nextbuy.cart.util.Constant.CART_NOT_FOUND;
-
 @Service
 public class CartService {
   private static final String INVALID_USER = "invalid user";
@@ -54,6 +52,7 @@ public class CartService {
   }
 
   public Cart createCart(CartDTO cartDTO) {
+    BigDecimal total = BigDecimal.ZERO;
     validateUser(cartDTO);
     var items = itemGateway.getItems(cartDTO.itemsIds()
             .stream()
@@ -69,10 +68,16 @@ public class CartService {
       throw new BadRequestException(THERE_IS_ALREADY_PENDING_CART);
     }
 
+    for (Item item : cartDTO.itemsIds()) {
+      var quantity = item.getQuantity();
+      var price = items.results().stream().filter(it -> it.id().equals(item.getId())).findFirst().orElseThrow(() -> new NotFoundException(ITEM_NOT_FOUND)).price();
+
+      total = total.add(price.multiply(new BigDecimal(quantity)));
+    }
+
+
     var newCart = new Cart(cartDTO);
-    newCart.setTotal(items.results().stream()
-            .map(ItemDTO::price)
-            .reduce(BigDecimal.ZERO, BigDecimal::add));
+    newCart.setTotal(total);
 
     return cartRepository.save(newCart);
   }
@@ -84,7 +89,7 @@ public class CartService {
     }
 
     var cart = cartRepository.findByUserIdAndStatus(addItemDTO.userId(), Status.PENDING);
-    var total = itemGateway.getItems(List.of(addItemDTO.id()))
+    var price = itemGateway.getItems(List.of(addItemDTO.id()))
             .results()
             .stream()
             .findFirst()
@@ -92,21 +97,31 @@ public class CartService {
             .orElseThrow(() -> new NotFoundException(ITEM_NOT_FOUND));
 
     cart.setItemsIds(List.of(new Item(addItemDTO.id(), addItemDTO.quantity())));
-    cart.setTotal(cart.getTotal().add(total));
+    cart.setTotal(cart.getTotal().add(price.multiply(new BigDecimal(addItemDTO.quantity()))));
 
     return cartRepository.save(cart);
   }
 
   public void removeItemFromCart(Long cartId, String itemId) {
     var cart = cartRepository.findById(cartId);
+//    var itemPrice = itemGateway.getItems(List.of(itemId))
+//            .results().stream()
+//            .map(ItemDTO::price)
+//            .reduce(BigDecimal.ZERO, BigDecimal::add);
+
     var itemPrice = itemGateway.getItems(List.of(itemId))
             .results().stream()
             .map(ItemDTO::price)
             .reduce(BigDecimal.ZERO, BigDecimal::add);
 
+
+
+
     cart.ifPresent(it -> {
+      var itemResult = it.getItemsIds().stream().filter(item -> item.getId().equals(itemId)).findFirst().orElseThrow(() -> new NotFoundException(ITEM_NOT_FOUND));
       it.getItemsIds().removeIf(item -> item.getId().equals(itemId));
-      it.setTotal(it.getTotal().subtract(itemPrice));
+
+      it.setTotal(it.getTotal().subtract(itemPrice.multiply(new BigDecimal(itemResult.getQuantity()))));
       cartRepository.save(it);
     });
 
